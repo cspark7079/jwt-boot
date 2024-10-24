dcbd118d17c181f21d7bcc2829a0e2ee022246ea
cspark7079 <cs.park7079@gmail.com>
2024-10-24 19:43:30 +0900

    main 추가

---
+++ src/test/java/com/cspark/jwt_boot/JwtBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cspark.jwt_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
